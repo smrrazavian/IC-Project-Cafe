10714f3672ff23499c724dc7dfcd4084a9a0d88f
smrrazavian <smrrazavian@outlook.com>
2022-01-28 17:49:20 +0330

    Final Version, Ready to release.

--- CafeProjectUni.py
+++ CafeProjectUni.py
@@ -1,9 +1,19 @@
+"""Cafe Project Code for KHU Intro to computer course"""
+# --------------------------- Import libraries and functions --------------------------
 import numpy as np
 import random
 
-
+# -------------------------------- Cafe Class --------------------------------
 class Cafe:
-    def __init__(self, name):
+    """Cafe Class"""
+
+    def __init__(self, name: str) -> None:
+        """Main function for Cafe Class.
+
+        Args:
+            Self: self represents the instance of the class.
+            name (Type: string): Name of the customer.
+        """
         self.name = name
         self.menu = {
             "Mocha": 4.25,
@@ -18,15 +28,28 @@ class Cafe:
         self.total_orders = []
         self.total_price = 0
 
-    def request_menu(self):
-        print(self.menu)
+    def request_menu(self) -> str:
+        """This function prints menu items."""
+        [print(key, ":", value) for key, value in self.menu.items()]
 
-    def place_order(self, order):
+    def place_order(self, order: str) -> None:
+        """A function to save prices and orders if the orders exists in self.menu.
+
+        Args:
+            Self: self represents the instance of the class.
+            order (Type: string): orders.
+        """
         if order in self.menu:
             self.total_orders.append(order)
             self.total_price += self.menu[order]
 
-    def request_bill(self, discount):
+    def request_bill(self, discount: float) -> str:
+        """This function prints bill
+
+        Args:
+            Self: self represents the instance of the class.
+            discount (Type: float): discount of bill that is determined from game.
+        """
         values, counts = np.unique(self.total_orders, return_counts=True)
         final_cost = self.total_price * (1 - discount)
         print("============================")
@@ -42,6 +65,7 @@ class Cafe:
         print("============================")
 
 
+# -------------------------------- Main App --------------------------------
 name = input("Hi!\nWelcome to our Cafe!\nWould you please enter your name?\n")
 customer = Cafe(name)
 menu_req = input("Do you want to see the menu?\n").lower()
@@ -60,6 +84,7 @@ while True:
         game_req = input("Do you want to play a game? \n").lower()
         discount = 0
         if game_req == "yes":
+            # -------------------------------- Game --------------------------------
             x_loc = random.randint(1, 10)
             y_loc = random.randint(1, 10)
             counter = 0
