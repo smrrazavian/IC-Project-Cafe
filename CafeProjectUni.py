9c3b7c5bf444235da4426b36fa286ca680b70990
smrrazavian <smrrazavian@outlook.com>
2022-01-28 16:24:38 +0330

    Semi final:))

--- CafeProjectUni.py
+++ CafeProjectUni.py
@@ -1,13 +1,9 @@
-"""This is a final project for KHU-University Introduction of Programming course."""
+import numpy as np
 import random
 
 
 class Cafe:
-    def __init__(self, name) -> None:
-        """This is the main function of Cafe's class and It contains menu and customer's name.
-        Args:
-        Returns:
-        """
+    def __init__(self, name):
         self.name = name
         self.menu = {
             "Mocha": 4.25,
@@ -22,30 +18,39 @@ class Cafe:
         self.total_orders = []
         self.total_price = 0
 
-    def request_menu(self) -> dict:
-        """ """
+    def request_menu(self):
         print(self.menu)
 
-    def place_order(self, order) -> None:
-        """ """
+    def place_order(self, order):
         if order in self.menu:
             self.total_orders.append(order)
             self.total_price += self.menu[order]
 
     def request_bill(self, discount):
+        values, counts = np.unique(self.total_orders, return_counts=True)
         final_cost = self.total_price * (1 - discount)
+        print("============================")
         print(f"Your name is: {self.name}")
         print("Your Bill : ")
+        temp = 0
+        while temp < len(values):
+            print("    ", values[temp], ":", counts[temp])
+            temp += 1
         print(f"Total cost before discount: {self.total_price}")
         print(f"Your final cost: {final_cost}")
         print("Bye Bye!")
+        print("============================")
 
 
 name = input("Hi!\nWelcome to our Cafe!\nWould you please enter your name?\n")
 customer = Cafe(name)
-menu_req = input("Do you want to see the menu?").lower()
+menu_req = input("Do you want to see the menu?\n").lower()
 if menu_req == "yes":
-    customer.request_menu()  # Todo : handle menu req.
+    customer.request_menu()
+elif menu_req == "no":
+    pass
+else:
+    raise Exception("Please Enter valid input!")
 while True:
     ordered = input("What do you want?: \n")
     order_status = True
@@ -53,24 +58,26 @@ while True:
     if ordered == "place order" or ordered == "Place order":
         print("Ok! Please wait until your orders get ready!")
         game_req = input("Do you want to play a game? \n").lower()
+        discount = 0
         if game_req == "yes":
             x_loc = random.randint(1, 10)
             y_loc = random.randint(1, 10)
             counter = 0
+            print(
+                "Welcome to the game!\nWe have a hidden ping-pong ball and you"
+                "have to guess the ball's location like: x,y \n"
+                "Note : the location's points is in the range of [1,10]: \n"
+            )
             while True:
-                game_loc = input(
-                    "Welcome to the game!\nWe have a hidden ping-pong ball and you"
-                    "have to guess the ball's location like: x,y \n"
-                    "Note : the location's points is in the range of [1,10]: \n"
-                )
+                game_loc = input()
                 if game_loc == "end game":
-                    break
+                    customer.request_bill(0)
+                    exit()
                 game_loc = game_loc.split(",")
                 x = int(game_loc[0])
                 y = int(game_loc[1])
-                guess_status = ""
+                guess_status = "Hint: "
                 game_done = False
-                discount = 0
                 if x > x_loc:
                     guess_status += "Left "
                 elif x < x_loc:
@@ -82,9 +89,9 @@ while True:
                 if y_loc == y and x_loc == x:
                     print("You nailed it! Success!")
                     game_done = True
-                    break
+                if game_done == False:
+                    print(guess_status + "\nTry again:")
                 counter += 1
-                print(guess_status)
                 if counter > 6:
                     print("Bruh! The game is over! gg!")
                 if game_done == True:
@@ -94,4 +101,8 @@ while True:
                         discount = 0.5
                     elif counter <= 6 and counter > 4:
                         discount = 0.1
-                customer.request_bill(discount)
+                    customer.request_bill(discount)
+                    exit()
+        if game_req == "no":
+            customer.request_bill(discount)
+            exit()
